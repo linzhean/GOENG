71f22fb2d4a5473beeb4f3be05693b269051249f
William02200 <153977523+William02200@users.noreply.github.com>
2024-01-07 16:40:05 +0800

    修改 main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/gestures.dart';
+import 'dart:ui';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:goeng/utils.dart';
+import 'package:goeng/pages/singleWord_LR.dart';
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+	@override
+	Widget build(BuildContext context) {
+	return MaterialApp(
+		title: 'Flutter',
+		debugShowCheckedModeBanner: false,
+		scrollBehavior: MyCustomScrollBehavior(),
+		theme: ThemeData(
+		primarySwatch: Colors.blue,
+		),
+		home: Scaffold(
+		body: SingleChildScrollView(
+			child: Scene(),
+		),
+		),
+	);
+	}
+}
+
+
+/***import 'package:flutter/material.dart';
 import 'package:goeng/pages/HomePage.dart';
 
 void main() => runApp(
       const MaterialApp(
         home: HomePage(),
       ),
-    );
+    );***/
+
+
+
